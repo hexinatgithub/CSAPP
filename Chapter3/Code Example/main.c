1b0875a4cd7c90d228f39c862d9a6e059c033f5f
hexin <hexiningame@126.com>
2015-07-19 18:05:36 +0800

    complete Chapter3

--- Chapter3/Code Example/main.c
+++ Chapter3/Code Example/main.c
@@ -1,5 +1,20 @@
+#include <stdio.h>
+
+int sum(int a, int b) {
+	int s = a + b;
+	printf("%p\n", &s);
+	return s;
+}
+
 int main(int argc, char const *argv[])
 {
-	return sum(1, 3);
+	// return sum(1, 3);
+	// int a = 10;
+	// printf("%p\n", &a);
+	// int b = 20;
+	// int s = sum(a, b);
+	// printf("%d\n", s);
+	long int hehe = 9223372036854775807;
+	printf("%lx\n", hehe);
 	return 0;
 }
